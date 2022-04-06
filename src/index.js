7d44bcb0eb8cdeab7546198478c433f8e4dc53d0
ACLXRD <andres.lopez01@correo.usa.edu.co>
2022-04-06 00:37:04 -0500

    Simulación funcional, pero no para caracteres especiales

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 let groupList = document.getElementById("segmetnList");
 let muxList = document.getElementById("muxList");
 let visible = document.getElementsByClassName('post');
+let muxhtml = document.getElementById("muxLista");
 
 function getMessage(){
-
+    document.getElementById("alertInst").style.display = "none";
     groupList.innerHTML = "";
+    muxList.innerHTML = "";
 
     let txt = document.getElementById("message").value;
     if(txt == ""){
@@ -22,17 +24,23 @@ function getMessage(){
  *  @returns The @param txt converted to binary
  */
 function toBinary(txt, type = 0){
-    let binary = "";
-    //ASCII/UTF-8
-    for (let i = 0; i < txt.length; i++) {
-        binary += txt[i].charCodeAt(0).toString(2) + " ";
-    }
-
     // The txt to convert is a number
     if (type === 1) {   
         return txt.toString(2)
     }
-    
+
+    let binary = "";
+
+    txt = unescape(encodeURIComponent(txt));
+    var chr = 0, l = txt.length;
+    for( let i = 0; i < l; i ++ ){
+        chr = txt.charCodeAt(i).toString(2);
+        while( chr.length % 8 != 0 ){
+            chr = '0' + chr;
+        }
+        binary += chr;
+    }
+
     return binary;
 }
 
@@ -71,11 +79,16 @@ function copyText() {
 }
 
 function segmentation(txt){
-    let segments = txt.split(" ");
+    let segments = []
     let segmentsWID = [];
-   
     let c = 0;
     let ul = document.createElement('ul');
+
+    for (let i = 0; i < txt.length; i += 8) {
+        segments.push(txt.substring(i,i+8));
+    }
+    console.log(segments);
+
     ul.setAttribute('class','list-group list-group-horizontal m-auto');    
     groupList.appendChild(ul);
 
@@ -101,10 +114,9 @@ function sendPackage(segments){
     let numbers = Array.apply(null, {length: segments.length}).map(Number.call, Number)
     numbers = numbers.sort(function() {return Math.random() - 0.5});
     let unorderSegments = [];
-    console.log(numbers);
-    
+        
     while (numbers.length > 0) {
-        unorderSegments.push(segments[numbers.pop()]);
+        unorderSegments.push(segments[numbers.pop()]);       
     }
 
     let c = 0;
@@ -126,21 +138,63 @@ function sendPackage(segments){
         ul.appendChild(li);
     }
 
-
     mux(unorderSegments);
 }
 
 function mux(unorderSegments){
-    let orderSegments = []; 
+    let orderSegments = [];    
+    let c = 0;
+    let mx = document.createElement('ul');
+
     for (const item of unorderSegments) {
+        
         let idLen = item.length - 7;
         let seg = {
             id: BinarioADecimal(item.substring(0,idLen)),
             txt: item.substring(idLen, item.length),
         }
         orderSegments.push(seg);
-   }
+    }
+   
    
    orderSegments = orderSegments.sort((a, b) => (a.id > b.id) ? 1 : -1)
-   console.log(orderSegments);
+   
+   mx.setAttribute('class','list-group list-group-horizontal m-auto'); 
+   muxhtml.appendChild(mx);
+
+    for (let i = 0; i < orderSegments.length; i++) {
+        c++;
+        if(c === 7){
+            mx = document.createElement('ul');
+            mx.setAttribute('class','list-group list-group-horizontal m-auto');
+            muxhtml.appendChild(mx);
+            c = 0;    
+        }
+        let MU = document.createElement("li");
+        MU.appendChild(document.createTextNode(orderSegments[i].txt));
+        MU.setAttribute("class", "list-group-item");
+        mx.appendChild(MU);
+    }
+
+
+   MsgFinal(orderSegments);
+}
+
+function MsgFinal(orderSegments){
+    var result = "";
+    let cadena = "";
+    const mensajebin = orderSegments.map(function(msg) {
+        return msg.txt;
+    });
+
+  for (let i = 0; i < mensajebin.length; i++) {
+     cadena += mensajebin[i] + " ";
+  }
+
+  result = cadena.split(" ").map(function(elem) {
+    return String.fromCharCode(parseInt(elem, 2));
+  }).join("")
+
+  console.log(result);
+  document.getElementById('messageBinEnd').value = result;
 }
